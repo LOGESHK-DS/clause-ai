e10210f8a74bf8cfe5494f7084887d5771bca5dd
LOGESHK-DS <logeshds047@gmail.com>
2026-04-24 20:50:42 +0530

    Removed archive folder for cleaner structure

--- archive/retrieval/ingest.py
+++
@@ -1,4 +0,0 @@
-from vectorstore.chroma_store import initialize_chroma
-
-if __name__ == "__main__":
-    initialize_chroma()
--- archive/test_chroma.py
+++
@@ -1,8 +0,0 @@
-from vectorstore.chroma_store import load_chroma
-
-vs = load_chroma()
-
-all_meta = vs._collection.get(include=["metadatas"])["metadatas"]
-
-unique_contract_types = sorted(set(m["contract_type"] for m in all_meta))
-print(unique_contract_types)
